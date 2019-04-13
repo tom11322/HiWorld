9c9c0086df476f0f1c156a10489dc35d4dae34c8
Wade <liwade13@gmail.com>
2019-04-13 11:06:51 -0400

    commit

--- ParseChat/AppDelegate.swift
+++ ParseChat/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+            configuration.applicationId = "myAppId"
+            configuration.server = "http://chatgps.herokuapp.com/parse"
+        }))
         return true
     }
 
---
+++ ParseChat/FriendViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FriendViewController.swift
+//  ParseChat
+//
+//  Created by Wade Li on 4/8/19.
+//  Copyright © 2019 Wade Li. All rights reserved.
+//
+
+import UIKit
+
+class FriendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
